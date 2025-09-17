f606e5064a1e89f74b9c086b15fcd230c3ef5465
Markus Pettersson <markus.pettersson@mullvad.net>
2025-09-17 08:29:43 +0200

    Add `conform.nvim` / auto formatting on save

--- init.lua
+++ init.lua
@@ -18,6 +18,8 @@ vim.pack.add {
 	{ src = 'https://github.com/mrcjkb/rustaceanvim' },
 	-- Highlight TODO-esque comments
 	{ src = 'https://github.com/folke/todo-comments.nvim' }, -- depends on plenary.nvim
+	-- Formatting
+	{ src = 'https://github.com/stevearc/conform.nvim' },
 }
 
 
@@ -68,6 +70,18 @@ vim.lsp.config('lua_ls', {
 			},
 		},
 	},
+})
+
+require("conform").setup({
+  formatters_by_ft = {
+    lua = { "stylua" },
+    rust = { "rustfmt", lsp_format = "fallback" },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 })
 
 require('fidget').setup()
